616a58fc6caadf5081ab944759c4702588b836ab
alanpaul123 <alanpauldxb@gmail.com>
2024-11-27 12:52:27 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import TeachDash from "./Pages/TeachDash";
 
 import Footer from "./Components/Footer";
 import StudentDashboard from "./Pages/StudentDashboard";
+import Landing from "./Components/Landing";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/teachDash" element={<TeachDash />}></Route>
         <Route path="/studentDash" element={<StudentDashboard />}></Route>
+        <Route path="/home" element={<Landing />}></Route>
       </Routes>
       <Footer />
     </>
